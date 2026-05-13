b12e1f663adcf3b75a42a02b5a6432d0be2a2560
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2026-05-13 11:30:30 +0200

    fix: Package Name for Google Console

--- src/main/kotlin/com/gadijkh/MainActivity.kt
+++ src/main/kotlin/com/gadijkh/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.stillfresh
+package com.gadijkh
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.stillfresh.ui.theme.StillFreshTheme
+import com.gadijkh.ui.theme.StillFreshTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- src/main/kotlin/com/gadijkh/ui/theme/Theme.kt
+++ src/main/kotlin/com/gadijkh/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.stillfresh.ui.theme
+package com.gadijkh.ui.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicLightColorScheme
